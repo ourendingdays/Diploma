405f7c536fc0e503c7c99b726d5fa965296b6bbe
Paul Mospan <pavel.mospan@yahoo.com>
2019-08-30 16:59:44 +0300

    Data: geebo: finished scrapping and renamed some files

--- data/greebo/greebo/spiders/geebo_spider_real_estate.py
+++ data/greebo/greebo/spiders/geebo_spider_real_estate.py
@@ -3,14 +3,14 @@ from ..items import GeeboItem
 
 
 # inheriting from class scrapy.Spider
-class GeeboSpiderJobs(scrapy.Spider):
-    name = 'geebo_spider_equipment'
+class GeeboSpiderRealEstate(scrapy.Spider):
+    name = 'geebo_spider_real_estate'
     page_number = 2
     # site has 50 pages of equipment
     page_number_total = 50
 
     # list of websites we want to scrap
-    start_urls = ['https://geebo.com/construction-farm-equipment/list/mobile//page/1/']
+    start_urls = ['https://geebo.com/real-estate/list/mobile//page/1/']
 
     # response contains a source code of the web page we are scrapping (from start_url)
     def parse(self, response):
@@ -18,12 +18,11 @@ class GeeboSpiderJobs(scrapy.Spider):
         item = GeeboItem()
 
         articles = response.css(".image+ td")
-        next_page = response.css(".paging a::attr(href)").get()
 
         for article in articles:
             ad = article.css(".title::text").extract()
-            ad_href = article.css(".title::attr(href)").get()
-            print('ad_href: ', ad_href)
+            # ad_href = article.css(".title::attr(href)").get()
+            # print('ad_href: ', ad_href)
 
             item['ad'] = ad
 
@@ -31,8 +30,8 @@ class GeeboSpiderJobs(scrapy.Spider):
             # yield works with generator
             yield item
 
-        next_page = 'https://geebo.com/construction-farm-equipment/list/mobile//page/' + str(GeeboSpiderJobs.page_number) + '/'
-        if GeeboSpiderJobs.page_number <= GeeboSpiderJobs.page_number_total:
-            GeeboSpiderJobs.page_number += 1
+        next_page = 'https://geebo.com/real-estate/list/mobile//page/' + str(GeeboSpiderRealEstate.page_number) + '/'
+        if GeeboSpiderRealEstate.page_number <= GeeboSpiderRealEstate.page_number_total:
+            GeeboSpiderRealEstate.page_number += 1
             # callback tells Scrapy what to do next
             yield response.follow(next_page, callback=self.parse)
--- data/greebo/greebo/spiders/geebo_spider_services.py
+++ data/greebo/greebo/spiders/geebo_spider_services.py
@@ -3,14 +3,14 @@ from ..items import GeeboItem
 
 
 # inheriting from class scrapy.Spider
-class GeeboSpiderJobs(scrapy.Spider):
-    name = 'geebo_spider_equipment'
+class GeeboSpiderServices(scrapy.Spider):
+    name = 'geebo_spider_services'
     page_number = 2
     # site has 50 pages of equipment
     page_number_total = 50
 
     # list of websites we want to scrap
-    start_urls = ['https://geebo.com/construction-farm-equipment/list/mobile//page/1/']
+    start_urls = ['https://geebo.com/services/list/mobile//page/1/']
 
     # response contains a source code of the web page we are scrapping (from start_url)
     def parse(self, response):
@@ -18,12 +18,11 @@ class GeeboSpiderJobs(scrapy.Spider):
         item = GeeboItem()
 
         articles = response.css(".image+ td")
-        next_page = response.css(".paging a::attr(href)").get()
 
         for article in articles:
             ad = article.css(".title::text").extract()
-            ad_href = article.css(".title::attr(href)").get()
-            print('ad_href: ', ad_href)
+            # ad_href = article.css(".title::attr(href)").get()
+            # print('ad_href: ', ad_href)
 
             item['ad'] = ad
 
@@ -31,8 +30,8 @@ class GeeboSpiderJobs(scrapy.Spider):
             # yield works with generator
             yield item
 
-        next_page = 'https://geebo.com/construction-farm-equipment/list/mobile//page/' + str(GeeboSpiderJobs.page_number) + '/'
-        if GeeboSpiderJobs.page_number <= GeeboSpiderJobs.page_number_total:
-            GeeboSpiderJobs.page_number += 1
+        next_page = 'https://geebo.com/services/list/mobile//page/' + str(GeeboSpiderServices.page_number) + '/'
+        if GeeboSpiderServices.page_number <= GeeboSpiderServices.page_number_total:
+            GeeboSpiderServices.page_number += 1
             # callback tells Scrapy what to do next
             yield response.follow(next_page, callback=self.parse)
