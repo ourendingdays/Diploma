a3dd16a6ab9216c17e1086727992bac019528040
Paul Mospan <pavel.mospan@yahoo.com>
2019-09-12 13:14:39 +0300

    Data: amazon: scrapping further

--- data/amazon/amazon/spiders/amazon_spider_arts.py
+++ data/amazon/amazon/spiders/amazon_spider_arts.py
@@ -4,10 +4,19 @@ from ..items import AmazonItem
 
 class AmazonSpiderArts(scrapy.Spider):
     name = 'amazon_spider_art'
+    # start_urls = [
+    #     'https://www.amazon.com/s?i=arts-crafts-intl-ship&rh=n%3A4954955011&qid=1568238763&ref=sr_pg_1'
+    # ]
+    # start_urls = [
+    #     'https://www.amazon.com/Arts-Crafts/s?rh=n%3A4954955011&page=53'
+    # ]
+    # start_urls = [
+    #     'https://www.amazon.com/Arts-Crafts/s?rh=n%3A4954955011&page=70'
+    # ]
     start_urls = [
-        'https://www.amazon.com/Arts-Crafts/s?rh=n%3A4954955011&page=53'
+        'https://www.amazon.com/Arts-Crafts/s?rh=n%3A4954955011&page=140'
     ]
-    page_number = 54
+    page_number = 141
     page_number_total = 400
 
     def parse(self, response):
