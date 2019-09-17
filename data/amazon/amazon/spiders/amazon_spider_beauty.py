1d7c045e5bc7c45db4fb2549d84f13af75309b02
Paul Mospan <pavel.mospan@yahoo.com>
2019-09-17 23:18:26 +0300

    Data: amazon: beauty : started scrapping

---
+++ data/amazon/amazon/spiders/amazon_spider_beauty.py
@@ -0,0 +1,23 @@
+import scrapy
+from ..items import AmazonItem
+
+
+class AmazonSpiderBeauty(scrapy.Spider):
+    name = 'amazon_spider_beauty'
+
+    # start_urls = ['https://www.amazon.com/s?i=beauty-intl-ship&rh=n%3A16225006011&page=2&qid=1568750157&ref=sr_pg_2']
+    start_urls = ['https://www.amazon.com/s?rh=n%3A16225006011&page=10']
+
+    def parse(self, response):
+        items = AmazonItem()
+        next_page_real = response.css(".a-last a").css("::attr(href)").get()
+
+        ad = response.css('.a-color-base.a-text-normal::text').extract()
+
+        for i in range(len(ad)):
+            items['ad'] = ad[i]
+
+            yield items
+
+        if next_page_real is not None:
+            yield response.follow(next_page_real, callback=self.parse)
