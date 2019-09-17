b64522fa5ca076d83615b728a0f946bc8437bfe5
Paul Mospan <pavel.mospan@yahoo.com>
2019-09-17 20:45:58 +0300

    Data: amazon: automotive: scrapped completely

--- data/amazon/amazon/spiders/amazon_spider_automotive.py
+++ data/amazon/amazon/spiders/amazon_spider_automotive.py
@@ -6,7 +6,7 @@ class AmazonSpiderAutomotive(scrapy.Spider):
     name = 'amazon_spider_automotive'
 
     # start_urls = ['https://www.amazon.com/s?rh=n%3A2562090011&page=2&qid=1568730935&ref=lp_2562090011_pg_2']
-    start_urls = ['https://www.amazon.com/Automotive/s?rh=n%3A2562090011&page=236']
+    start_urls = ['https://www.amazon.com/Automotive/s?rh=n%3A2562090011&page=390']
 
     def parse(self, response):
         items = AmazonItem()
