454dba09f491e21ffdd664bf4ff433b99dc0e29e
Paul Mospan <pavel.mospan@yahoo.com>
2019-09-17 19:38:51 +0300

    Data: amazon: automotive: scrapping

---
+++ data/amazon/amazon/spiders/amazon_spider_automotive.py
@@ -0,0 +1,23 @@
+import scrapy
+from ..items import AmazonItem
+
+
+class AmazonSpiderAutomotive(scrapy.Spider):
+    name = 'amazon_spider_automotive'
+
+    # start_urls = ['https://www.amazon.com/s?rh=n%3A2562090011&page=2&qid=1568730935&ref=lp_2562090011_pg_2']
+    start_urls = ['https://www.amazon.com/Automotive/s?rh=n%3A2562090011&page=236']
+
+    def parse(self, response):
+        items = AmazonItem()
+        next_page_real = response.css(".a-last a").css("::attr(href)").get()
+
+        ad = response.css('.a-color-base.a-text-normal::text').extract()
+
+        for i in range(len(ad)):
+            items['ad'] = ad[i]
+
+            yield items
+
+        if next_page_real is not None:
+            yield response.follow(next_page_real, callback=self.parse)
