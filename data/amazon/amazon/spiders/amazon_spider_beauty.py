84a7dafa905817ae5eac3acab6db86deed59de8d
Paul Mospan <pavel.mospan@yahoo.com>
2019-09-18 22:38:34 +0300

    Data: amazon: beauty: scrapped more and merged together

--- data/amazon/amazon/spiders/amazon_spider_beauty.py
+++ data/amazon/amazon/spiders/amazon_spider_beauty.py
@@ -6,7 +6,7 @@ class AmazonSpiderBeauty(scrapy.Spider):
     name = 'amazon_spider_beauty'
 
     # start_urls = ['https://www.amazon.com/s?i=beauty-intl-ship&rh=n%3A16225006011&page=2&qid=1568750157&ref=sr_pg_2']
-    start_urls = ['https://www.amazon.com/s?rh=n%3A16225006011&page=110']
+    start_urls = ['https://www.amazon.com/s?rh=n%3A16225006011&page=360']
 
     def parse(self, response):
         items = AmazonItem()
