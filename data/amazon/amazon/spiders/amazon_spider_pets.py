0c6809a3b3446a5cc48327cab5d84d1b331d2713
Paul Mospan <pavel.mospan@yahoo.com>
2019-09-20 21:19:00 +0300

    Data: amazon: pets: dogs scrapped

--- data/amazon/amazon/spiders/amazon_spider_pets.py
+++ data/amazon/amazon/spiders/amazon_spider_pets.py
@@ -10,7 +10,7 @@ class AmazonSpiderPets(scrapy.Spider):
     #     'https://www.amazon.com/s?i=pets-intl-ship&bbn=16225013011&rh=n%3A16225013011%2Cn%3A2975312011&dc&page=3&fst=as%3Aoff&pf_rd_i=16225013011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=840ea3ca-218c-4921-bdeb-ab06a7a27a3c&pf_rd_r=CN31HGPMYAJC96MZMTDV&pf_rd_s=merchandised-search-2&pf_rd_t=101&qid=1568997582&rnid=16225013011&ref=sr_pg_2'
     # ]
     start_urls = [
-       'https://www.amazon.com/Dogs-Pet-Supplies/s?rh=n%3A2975312011&page=215'
+       'https://www.amazon.com/Dogs-Pet-Supplies/s?rh=n%3A2975312011&page=335'
     ]
 
     def parse(self, response):
