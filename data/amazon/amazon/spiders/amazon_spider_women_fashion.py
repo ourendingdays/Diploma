0965d5645d0ff158207fc50c7e4324ad147e6cfc
Paul Mospan <pavel.mospan@yahoo.com>
2019-09-22 01:49:14 +0300

    Data: amazon: scrapped sweaters and scrapping hoodies

--- data/amazon/amazon/spiders/amazon_spider_women_fashion.py
+++ data/amazon/amazon/spiders/amazon_spider_women_fashion.py
@@ -8,7 +8,9 @@ class AmazonSpiderWomenFashion(scrapy.Spider):
 
     # start_urls = ['https://www.amazon.com/s?i=fashion-womens-intl-ship&bbn=16225018011&rh=n%3A16225018011%2Cn%3A1040660%2Cn%3A1045024&dc&page=2&pf_rd_i=16225018011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=e9a7a2cd-d373-460c-8c25-702b5e2acb03&pf_rd_r=8QEXM8WHY2N3V0TEP07X&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1569064421&rnid=1040660&ref=sr_pg_2']
     # start_urls = ['https://www.amazon.com/Dresses-Clothing/s?rh=n%3A1045024&page=270']
-    start_urls = ['https://www.amazon.com/Tops-Tees-Blouses-Clothing/s?rh=n%3A2368343011&page=215']
+    # start_urls = ['https://www.amazon.com/Tops-Tees-Blouses-Clothing/s?rh=n%3A2368343011&page=215']
+    # start_urls = ['https://www.amazon.com/s?i=fashion-womens-intl-ship&bbn=16225018011&rh=n%3A16225018011%2Cn%3A1040660%2Cn%3A1044456&dc&page=2&pf_rd_i=16225018011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=e9a7a2cd-d373-460c-8c25-702b5e2acb03&pf_rd_r=8QEXM8WHY2N3V0TEP07X&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1569100636&rnid=1040660&ref=sr_pg_2']
+    start_urls = ['https://www.amazon.com/Fashion-Hoodies-Sweatshirts-Clothing/s?rh=n%3A1258603011&page=235']
 
 
     def parse(self, response):
