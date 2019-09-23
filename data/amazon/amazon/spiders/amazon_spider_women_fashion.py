065788cc6cb06d7cd083dd7069d2f2740e64b21f
Paul Mospan <pavel.mospan@yahoo.com>
2019-09-24 02:06:08 +0300

    Data: amazon: women fashion clothing: scrapped commom for 400 pages

--- data/amazon/amazon/spiders/amazon_spider_women_fashion.py
+++ data/amazon/amazon/spiders/amazon_spider_women_fashion.py
@@ -22,7 +22,8 @@ class AmazonSpiderWomenFashion(scrapy.Spider):
     # start_urls = ['https://www.amazon.com/Jumpsuits-Rompers-Overalls-Clothing/s?rh=n%3A9522930011&page=355']
     # start_urls = ['https://www.amazon.com/Coats-Jackets-Vests-Clothing/s?rh=n%3A1044646&page=275']
     # start_urls = ['https://www.amazon.com/s?i=fashion-womens-intl-ship&bbn=16225018011&rh=n%3A16225018011%2Cn%3A1040660%2Cn%3A9522932011&dc&page=2&pf_rd_i=16225018011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=0909843d-7d3a-40a7-8b35-6c3da8755bbd&pf_rd_p=0909843d-7d3a-40a7-8b35-6c3da8755bbd&pf_rd_r=4QH1R4R5T751ZQ1FKQAW&pf_rd_r=4QH1R4R5T751ZQ1FKQAW&pf_rd_s=merchandised-search-left-2&pf_rd_t=101&qid=1569270810&rnid=1040660&ref=sr_pg_2']
-    start_urls = ['https://www.amazon.com/Socks-Hosiery-Clothing/s?rh=n%3A1044886&page=255']
+    # start_urls = ['https://www.amazon.com/Socks-Hosiery-Clothing/s?rh=n%3A1044886&page=255']
+    start_urls = ['https://www.amazon.com/s?i=fashion-womens-intl-ship&bbn=16225018011&rh=n%3A7141123011%2Cn%3A16225018011%2Cn%3A1040660&page=2&pf_rd_i=16225018011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=0909843d-7d3a-40a7-8b35-6c3da8755bbd&pf_rd_p=0909843d-7d3a-40a7-8b35-6c3da8755bbd&pf_rd_r=7YWR4MMQ0JXW2JFXPGQ9&pf_rd_r=7YWR4MMQ0JXW2JFXPGQ9&pf_rd_s=merchandised-search-left-2&pf_rd_t=101&qid=1569277142&ref=sr_pg_2']
 
     def parse(self, response):
         items = AmazonItem()
