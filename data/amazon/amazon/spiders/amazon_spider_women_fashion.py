9d702d0634b4c646316cc7df138e0df364beac62
Paul Mospan <pavel.mospan@yahoo.com>
2019-09-23 23:33:00 +0300

    Data: amazon: scrapped overalls and jackets

--- data/amazon/amazon/spiders/amazon_spider_women_fashion.py
+++ data/amazon/amazon/spiders/amazon_spider_women_fashion.py
@@ -18,7 +18,9 @@ class AmazonSpiderWomenFashion(scrapy.Spider):
     # start_urls = ['https://www.amazon.com/s?i=fashion-womens-intl-ship&bbn=16225018011&rh=n%3A16225018011%2Cn%3A1040660%2Cn%3A1258967011&dc&page=2&pf_rd_i=16225018011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=e9a7a2cd-d373-460c-8c25-702b5e2acb03&pf_rd_r=8QEXM8WHY2N3V0TEP07X&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1569181277&rnid=1040660&ref=sr_pg_2']
     # start_urls = ['https://www.amazon.com/s?i=fashion-womens-intl-ship&bbn=16225018011&rh=n%3A16225018011%2Cn%3A1040660%2Cn%3A3456051&dc&page=2&pf_rd_i=16225018011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=0909843d-7d3a-40a7-8b35-6c3da8755bbd&pf_rd_p=0909843d-7d3a-40a7-8b35-6c3da8755bbd&pf_rd_r=4QH1R4R5T751ZQ1FKQAW&pf_rd_r=4QH1R4R5T751ZQ1FKQAW&pf_rd_s=merchandised-search-left-2&pf_rd_t=101&qid=1569228522&rnid=1040660&ref=sr_pg_2']
     # start_urls = ['https://www.amazon.com/Swimsuits-Cover-Ups-Clothing/s?rh=n%3A1046622&page=190']
-    start_urls = ['https://www.amazon.com/Lingerie-Sleep-Lounge-Clothing/s?rh=n%3A9522931011&page=270']
+    # start_urls = ['https://www.amazon.com/Lingerie-Sleep-Lounge-Clothing/s?rh=n%3A9522931011&page=270']
+    # start_urls = ['https://www.amazon.com/Jumpsuits-Rompers-Overalls-Clothing/s?rh=n%3A9522930011&page=355']
+    start_urls = ['https://www.amazon.com/Coats-Jackets-Vests-Clothing/s?rh=n%3A1044646&page=275']
 
     def parse(self, response):
         items = AmazonItem()
