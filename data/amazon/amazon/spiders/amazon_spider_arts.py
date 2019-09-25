66773c55aba765df55bcb48f310a25dc2fff07bf
Paul Mospan <pavel.mospan@yahoo.com>
2019-09-25 23:53:38 +0300

    Data: amazon: arts and crafts: printmaking scrapbooking sewing sewing storage

--- data/amazon/amazon/spiders/amazon_spider_arts.py
+++ data/amazon/amazon/spiders/amazon_spider_arts.py
@@ -29,7 +29,11 @@ class AmazonSpiderArts(scrapy.Spider):
     # start_urls = ['https://www.amazon.com/Fabric-Arts-Crafts-Sewing/s?rh=n%3A12899121&page=249']
     # start_urls = ['https://www.amazon.com/Fabric-Decorating-Arts-Crafts-Sewing/s?rh=n%3A12896841&page=220']
     # start_urls = ['https://www.amazon.com/s?i=arts-crafts-intl-ship&bbn=4954955011&rh=n%3A4954955011%2Cn%3A2617942011%2Cn%3A12897221&dc&page=2&fst=as%3Aoff&qid=1569415197&rnid=2617942011&ref=sr_pg_2']
-    start_urls = ['https://www.amazon.com/Needlework-Arts-Crafts-Sewing/s?rh=n%3A2237329011&page=260']
+    # start_urls = ['https://www.amazon.com/Needlework-Arts-Crafts-Sewing/s?rh=n%3A2237329011&page=260']
+    # start_urls = ['https://www.amazon.com/Organization-Storage-Transport-Arts-Crafts-Sewing/s?rh=n%3A2237594011&page=285']
+    # start_urls = ['https://www.amazon.com/Printmaking-Arts-Crafts-Sewing/s?rh=n%3A12898451&page=344']
+    # start_urls = ['https://www.amazon.com/s?i=arts-crafts-intl-ship&bbn=4954955011&rh=n%3A4954955011%2Cn%3A2617942011%2Cn%3A12898821&dc&page=2&fst=as%3Aoff&qid=1569438393&rnid=2617942011&ref=sr_pg_2']
+    start_urls = ['https://www.amazon.com/s?i=arts-crafts-intl-ship&bbn=4954955011&rh=n%3A4954955011%2Cn%3A2617942011%2Cn%3A12899091&dc&page=2&fst=as%3Aoff&qid=1569441489&rnid=2617942011&ref=sr_pg_2']
     # page_number = 316
     # page_number_total = 400
 
@@ -44,7 +48,7 @@ class AmazonSpiderArts(scrapy.Spider):
             yield items
 
         if next_page_real is not None:
-            time.sleep(10)
+            time.sleep(5)
             yield response.follow(next_page_real, callback=self.parse)
 
         # next_page = 'https://www.amazon.com/Arts-Crafts/s?rh=n%3A4954955011&page=' + str(AmazonSpiderArts.page_number)
