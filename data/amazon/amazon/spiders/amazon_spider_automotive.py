7aa654e5124bf151926345f2adb915bf7714db8e
Paul Mospan <pavel.mospan@yahoo.com>
2019-09-27 02:12:05 +0300

    Data: amazon: automotive: rv performance and replacements parts

--- data/amazon/amazon/spiders/amazon_spider_automotive.py
+++ data/amazon/amazon/spiders/amazon_spider_automotive.py
@@ -15,7 +15,10 @@ class AmazonSpiderAutomotive(scrapy.Spider):
     # start_urls = ['https://www.amazon.com/Interior-Accessories-Automotive/s?i=automotive&rh=n%3A15857501&page=325']
     # start_urls = ['https://www.amazon.com/s?i=automotive-intl-ship&bbn=2562090011&rh=n%3A2562090011%2Cn%3A15690151%2Cn%3A346333011&dc&page=2&fst=as%3Aoff&qid=1569525379&rnid=15690151&ref=sr_pg_2']
     # start_urls = ['https://www.amazon.com/s?i=automotive-intl-ship&bbn=2562090011&rh=n%3A15718791&dc&page=66&fst=as%3Aoff&qid=1569528656&rnid=15690151&ref=sr_pg_63']
-    start_urls = ['https://www.amazon.com/s?i=automotive-intl-ship&bbn=2562090011&rh=n%3A2562090011%2Cn%3A15690151%2Cn%3A13591416011&dc&page=2&fst=as%3Aoff&qid=1569529020&rnid=15690151&ref=sr_pg_2']
+    # start_urls = ['https://www.amazon.com/s?i=automotive-intl-ship&bbn=2562090011&rh=n%3A2562090011%2Cn%3A15690151%2Cn%3A13591416011&dc&page=2&fst=as%3Aoff&qid=1569529020&rnid=15690151&ref=sr_pg_2']
+    # start_urls = ['https://www.amazon.com/s?i=automotive-intl-ship&bbn=2562090011&rh=n%3A2562090011%2Cn%3A15690151%2Cn%3A15710351&dc&page=2&fst=as%3Aoff&qid=1569529951&rnid=15690151&ref=sr_pg_2']
+    # start_urls = ['https://www.amazon.com/s?i=automotive-intl-ship&bbn=2562090011&rh=n%3A2562090011%2Cn%3A15690151%2Cn%3A15719731&dc&page=2&fst=as%3Aoff&qid=1569532675&rnid=15690151&ref=sr_pg_2']
+    start_urls = ['https://www.amazon.com/s?i=automotive-intl-ship&bbn=2562090011&rh=n%3A2258019011&dc&page=179&fst=as%3Aoff&qid=1569536609&rnid=15690151&ref=sr_pg_178']
 
     def parse(self, response):
         items = AmazonItem()
