7dc2b7249a111730a6f1e77c88adc408f533a48c
Paul Mospan <pavel.mospan@yahoo.com>
2019-09-26 16:15:13 +0300

    Data: amazon: automotive: car electronics care care exterior

--- data/amazon/amazon/settings.py
+++ data/amazon/amazon/settings.py
@@ -16,11 +16,42 @@ NEWSPIDER_MODULE = 'amazon.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'amazon (+http://www.yourdomain.com)'
+# USER_AGENT = 'amazon (+http://www.yourdomain.com)'
 # USER_AGENT = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
 # USER_AGENT = 'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
 # USER_AGENT = 'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; http://www.google.com/bot.html) Safari/537.36'
 # USER_AGENT = 'Googlebot/2.1 ( http://www.googlebot.com/bot.html)'
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+    'scrapy_useragents.downloadermiddlewares.useragents.UserAgentsMiddleware': 500,
+}
+
+USER_AGENTS = [
+    ('Mozilla/5.0 (X11; Linux x86_64) '
+     'AppleWebKit/537.36 (KHTML, like Gecko) '
+     'Chrome/57.0.2987.110 '
+     'Safari/537.36'),  # chrome
+    ('Mozilla/5.0 (X11; Linux x86_64) '
+     'AppleWebKit/537.36 (KHTML, like Gecko) '
+     'Chrome/61.0.3163.79 '
+     'Safari/537.36'),  # chrome
+    ('Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:55.0) '
+     'Gecko/20100101 '
+     'Firefox/55.0'),  # firefox
+    ('Mozilla/5.0 (X11; Linux x86_64) '
+     'AppleWebKit/537.36 (KHTML, like Gecko) '
+     'Chrome/61.0.3163.91 '
+     'Safari/537.36'),  # chrome
+    ('Mozilla/5.0 (X11; Linux x86_64) '
+     'AppleWebKit/537.36 (KHTML, like Gecko) '
+     'Chrome/62.0.3202.89 '
+     'Safari/537.36'),  # chrome
+    ('Mozilla/5.0 (X11; Linux x86_64) '
+     'AppleWebKit/537.36 (KHTML, like Gecko) '
+     'Chrome/63.0.3239.108 '
+     'Safari/537.36'),  # chrome
+]
 
 # PROXY_POOL_ENABLED = True
 #
--- data/amazon/amazon/spiders/amazon_spider_automotive.py
+++ data/amazon/amazon/spiders/amazon_spider_automotive.py
@@ -1,3 +1,5 @@
+import time
+
 import scrapy
 from ..items import AmazonItem
 
@@ -6,7 +8,11 @@ class AmazonSpiderAutomotive(scrapy.Spider):
     name = 'amazon_spider_automotive'
 
     # start_urls = ['https://www.amazon.com/s?rh=n%3A2562090011&page=2&qid=1568730935&ref=lp_2562090011_pg_2']
-    start_urls = ['https://www.amazon.com/Automotive/s?rh=n%3A2562090011&page=390']
+    # start_urls = ['https://www.amazon.com/Automotive/s?rh=n%3A2562090011&page=390']
+    # start_urls = ['https://www.amazon.com/Car-Care-Automotive/s?rh=n%3A15718271&page=250']
+    # start_urls = ['https://www.amazon.com/Car-Electronics-Accessories-Automotive/s?rh=n%3A2230642011&page=365']
+    # start_urls = ['https://www.amazon.com/Exterior-Accessories-Automotive/s?rh=n%3A15857511&page=323']
+    start_urls = ['https://www.amazon.com/Interior-Accessories-Automotive/s?rh=n%3A15857501&page=152']
 
     def parse(self, response):
         items = AmazonItem()
@@ -16,8 +22,8 @@ class AmazonSpiderAutomotive(scrapy.Spider):
 
         for i in range(len(ad)):
             items['ad'] = ad[i]
-
             yield items
 
         if next_page_real is not None:
+            time.sleep(5)
             yield response.follow(next_page_real, callback=self.parse)
