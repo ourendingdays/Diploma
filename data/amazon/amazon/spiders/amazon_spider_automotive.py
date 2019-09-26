8da5727249177d317921fc069ffc83cff8ba5998
Paul Mospan <pavel.mospan@yahoo.com>
2019-09-26 23:32:22 +0300

    Data: amazon: automotive: interior lights moto oils paint

--- data/amazon/amazon/spiders/amazon_spider_automotive.py
+++ data/amazon/amazon/spiders/amazon_spider_automotive.py
@@ -12,7 +12,10 @@ class AmazonSpiderAutomotive(scrapy.Spider):
     # start_urls = ['https://www.amazon.com/Car-Care-Automotive/s?rh=n%3A15718271&page=250']
     # start_urls = ['https://www.amazon.com/Car-Electronics-Accessories-Automotive/s?rh=n%3A2230642011&page=365']
     # start_urls = ['https://www.amazon.com/Exterior-Accessories-Automotive/s?rh=n%3A15857511&page=323']
-    start_urls = ['https://www.amazon.com/Interior-Accessories-Automotive/s?rh=n%3A15857501&page=152']
+    # start_urls = ['https://www.amazon.com/Interior-Accessories-Automotive/s?i=automotive&rh=n%3A15857501&page=325']
+    # start_urls = ['https://www.amazon.com/s?i=automotive-intl-ship&bbn=2562090011&rh=n%3A2562090011%2Cn%3A15690151%2Cn%3A346333011&dc&page=2&fst=as%3Aoff&qid=1569525379&rnid=15690151&ref=sr_pg_2']
+    # start_urls = ['https://www.amazon.com/s?i=automotive-intl-ship&bbn=2562090011&rh=n%3A15718791&dc&page=66&fst=as%3Aoff&qid=1569528656&rnid=15690151&ref=sr_pg_63']
+    start_urls = ['https://www.amazon.com/s?i=automotive-intl-ship&bbn=2562090011&rh=n%3A2562090011%2Cn%3A15690151%2Cn%3A13591416011&dc&page=2&fst=as%3Aoff&qid=1569529020&rnid=15690151&ref=sr_pg_2']
 
     def parse(self, response):
         items = AmazonItem()
