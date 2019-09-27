9fde7d009e39c31a9d88b70bfc3c0e258f413452
Paul Mospan <pavel.mospan@yahoo.com>
2019-09-28 00:57:14 +0300

    Data: amazon: beauty: skin-care nail-care makeup hair-care fragrance

--- data/amazon/amazon/spiders/amazon_spider_beauty.py
+++ data/amazon/amazon/spiders/amazon_spider_beauty.py
@@ -6,7 +6,12 @@ class AmazonSpiderBeauty(scrapy.Spider):
     name = 'amazon_spider_beauty'
 
     # start_urls = ['https://www.amazon.com/s?i=beauty-intl-ship&rh=n%3A16225006011&page=2&qid=1568750157&ref=sr_pg_2']
-    start_urls = ['https://www.amazon.com/s?rh=n%3A16225006011&page=385']
+    # start_urls = ['https://www.amazon.com/s?rh=n%3A16225006011&page=385']
+    # start_urls = ['https://www.amazon.com/s?i=beauty-intl-ship&bbn=16225006011&rh=n%3A16225006011%2Cn%3A11058281&dc&page=386&fst=as%3Aoff&qid=1569603195&rnid=16225006011&ref=sr_pg_386']
+    # start_urls = ['https://www.amazon.com/s?i=beauty-intl-ship&bbn=16225006011&rh=n%3A16225006011%2Cn%3A11060451&dc&page=267&fst=as%3Aoff&qid=1569614944&rnid=16225006011&ref=sr_pg_267']
+    # start_urls = ['https://www.amazon.com/s?i=beauty-intl-ship&bbn=16225006011&rh=n%3A16225006011%2Cn%3A11057241&dc&page=2&fst=as%3Aoff&qid=1569615651&rnid=16225006011&ref=sr_pg_2']
+    # start_urls = ['https://www.amazon.com/s?i=beauty-intl-ship&bbn=16225006011&rh=n%3A16225006011%2Cn%3A11056591&dc&page=2&fst=as%3Aoff&qid=1569616235&rnid=16225006011&ref=sr_pg_2']
+    start_urls = ['https://www.amazon.com/s?i=beauty-intl-ship&bbn=16225006011&rh=n%3A16225006011%2Cn%3A17242866011&dc&page=62&fst=as%3Aoff&qid=1569620079&rnid=16225006011&ref=sr_pg_62']
 
     def parse(self, response):
         items = AmazonItem()
