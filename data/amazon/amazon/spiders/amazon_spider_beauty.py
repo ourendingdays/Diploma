647c4532b86feb50c9202caec993b678792e2558
Paul Mospan <pavel.mospan@yahoo.com>
2019-09-28 19:16:12 +0300

    Data: amazon: beaty: completely

--- data/amazon/amazon/spiders/amazon_spider_beauty.py
+++ data/amazon/amazon/spiders/amazon_spider_beauty.py
@@ -1,3 +1,5 @@
+import time
+
 import scrapy
 from ..items import AmazonItem
 
@@ -11,7 +13,11 @@ class AmazonSpiderBeauty(scrapy.Spider):
     # start_urls = ['https://www.amazon.com/s?i=beauty-intl-ship&bbn=16225006011&rh=n%3A16225006011%2Cn%3A11060451&dc&page=267&fst=as%3Aoff&qid=1569614944&rnid=16225006011&ref=sr_pg_267']
     # start_urls = ['https://www.amazon.com/s?i=beauty-intl-ship&bbn=16225006011&rh=n%3A16225006011%2Cn%3A11057241&dc&page=2&fst=as%3Aoff&qid=1569615651&rnid=16225006011&ref=sr_pg_2']
     # start_urls = ['https://www.amazon.com/s?i=beauty-intl-ship&bbn=16225006011&rh=n%3A16225006011%2Cn%3A11056591&dc&page=2&fst=as%3Aoff&qid=1569616235&rnid=16225006011&ref=sr_pg_2']
-    start_urls = ['https://www.amazon.com/s?i=beauty-intl-ship&bbn=16225006011&rh=n%3A16225006011%2Cn%3A17242866011&dc&page=62&fst=as%3Aoff&qid=1569620079&rnid=16225006011&ref=sr_pg_62']
+    # start_urls = ['https://www.amazon.com/s?i=beauty-intl-ship&bbn=16225006011&rh=n%3A16225006011%2Cn%3A17242866011&dc&page=62&fst=as%3Aoff&qid=1569620079&rnid=16225006011&ref=sr_pg_62']
+    # start_urls = ['https://www.amazon.com/s?i=beauty-intl-ship&bbn=16225006011&rh=n%3A16225006011%2Cn%3A11062741&dc&page=285&fst=as%3Aoff&qid=1569622463&rnid=16225006011&ref=sr_pg_284']
+    # start_urls = ['https://www.amazon.com/s?i=beauty-intl-ship&bbn=16225006011&rh=n%3A16225006011%2Cn%3A3778591&dc&page=2&fst=as%3Aoff&qid=1569666720&rnid=16225006011&ref=sr_pg_2']
+    # start_urls = ['https://www.amazon.com/s?i=beauty-intl-ship&bbn=16225006011&dc&page=162&fst=as%3Aoff&qid=1569673192&rnid=16225006011&ref=sr_pg_162']
+    start_urls = ['https://www.amazon.com/s?i=beauty-intl-ship&bbn=16225006011&rh=n%3A16225006011%2Cn%3A10079992011&dc&page=4&fst=as%3Aoff&qid=1569676120&rnid=16225006011&ref=sr_pg_2']
 
     def parse(self, response):
         items = AmazonItem()
@@ -25,4 +31,5 @@ class AmazonSpiderBeauty(scrapy.Spider):
             yield items
 
         if next_page_real is not None:
+            time.sleep(5)
             yield response.follow(next_page_real, callback=self.parse)
