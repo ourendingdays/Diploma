50716e72335234762eef5489a5a800772f77c5d6
Paul Mospan <pavel.mospan@yahoo.com>
2019-09-29 02:04:00 +0300

    Data: amazon: computers: components and pc w/ tablets

--- data/amazon/amazon/spiders/amazon_spider_computers.py
+++ data/amazon/amazon/spiders/amazon_spider_computers.py
@@ -1,3 +1,4 @@
+import time
 import scrapy
 from ..items import AmazonItem
 
@@ -14,12 +15,15 @@ class AmazonSpiderComputers(scrapy.Spider):
     # start_urls = [
     #     'https://www.amazon.com/Computers/s?rh=n%3A16225007011&page=90'
     # ]
-    start_urls = [
-        'https://www.amazon.com/Computers/s?rh=n%3A16225007011&page=347'
-    ]
+    # start_urls = [
+    #     'https://www.amazon.com/Computers/s?rh=n%3A16225007011&page=347'
+    # ]
+    # start_urls = ['https://www.amazon.com/s?i=computers-intl-ship&bbn=16225007011&rh=n%3A16225007011%2Cn%3A193870011&dc&page=2&fst=as%3Aoff&qid=1569687504&rnid=16225007011&ref=sr_pg_2']
+    # start_urls = ['https://www.amazon.com/s?i=computers-intl-ship&bbn=16225007011&rh=n%3A16225007011%2Cn%3A13896617011&dc&page=2&fst=as%3Aoff&qid=1569695429&rnid=16225007011&ref=sr_pg_2']
+    start_urls = ['https://www.amazon.com/s?i=computers-intl-ship&bbn=16225007011&rh=n%3A16225007011%2Cn%3A1292110011&dc&page=331&fst=as%3Aoff&qid=1569700850&rnid=16225007011&ref=sr_pg_331']
 
-    page_number = 316
-    page_number_total = 400
+    # page_number = 316
+    # page_number_total = 400
 
     def parse(self, response):
         items = AmazonItem()
@@ -29,15 +33,15 @@ class AmazonSpiderComputers(scrapy.Spider):
 
         for i in range(len(ad)):
             items['ad'] = ad[i]
-
             yield items
+
+        if next_page_real is not None:
+            time.sleep(5)
+            yield response.follow(next_page_real, callback=self.parse)
+
+
 
         # next_page = 'https://www.amazon.com/Arts-Crafts/s?rh=n%3A4954955011&page=' + str(AmazonSpiderArts.page_number)
         # if AmazonSpiderArts.page_number <= AmazonSpiderArts.page_number_total:
         #     AmazonSpiderArts.page_number += 1
         #     yield response.follow(next_page, callback=self.parse)
-
-        if next_page_real is not None:
-            yield response.follow(next_page_real, callback=self.parse)
-
-
