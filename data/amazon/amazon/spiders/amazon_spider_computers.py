db95f367d626ccc64e1d568e35ce3b1abf53d072
Paul Mospan <pavel.mospan@yahoo.com>
2019-09-29 16:29:50 +0300

    Data: amazon: computers: power wprinters scanners servers

--- data/amazon/amazon/spiders/amazon_spider_computers.py
+++ data/amazon/amazon/spiders/amazon_spider_computers.py
@@ -23,8 +23,12 @@ class AmazonSpiderComputers(scrapy.Spider):
     #  start_urls = ['https://www.amazon.com/s?i=computers-intl-ship&bbn=16225007011&rh=n%3A16225007011%2Cn%3A1292110011&dc&page=331&fst=as%3Aoff&qid=1569700850&rnid=16225007011&ref=sr_pg_331']
     # start_urls = ['https://www.amazon.com/s?i=computers-intl-ship&bbn=16225007011&rh=n%3A16225007011%2Cn%3A3011391011&dc&page=292&fst=as%3Aoff&qid=1569716875&rnid=16225007011&ref=sr_pg_292']
     # start_urls = ['https://www.amazon.com/s?i=computers-intl-ship&bbn=16225007011&rh=n%3A16225007011%2Cn%3A1292115011&dc&page=2&fst=as%3Aoff&qid=1569717933&rnid=16225007011&ref=sr_pg_2']
-    start_urls = ['https://www.amazon.com/s?i=computers-intl-ship&bbn=16225007011&rh=n%3A16225007011%2Cn%3A172504&dc&page=2&fst=as%3Aoff&qid=1569751594&rnid=16225007011&ref=sr_pg_2']
-
+    # start_urls = ['https://www.amazon.com/s?i=computers-intl-ship&bbn=16225007011&rh=n%3A16225007011%2Cn%3A172504&dc&page=2&fst=as%3Aoff&qid=1569751594&rnid=16225007011&ref=sr_pg_2']
+    # start_urls = ['https://www.amazon.com/s?i=computers-intl-ship&bbn=16225007011&rh=n%3A16225007011%2Cn%3A17854127011&dc&page=2&fst=as%3Aoff&qid=1569755647&rnid=16225007011&ref=sr_pg_2']
+    # start_urls = ['https://www.amazon.com/s?i=computers-intl-ship&bbn=16225007011&rh=n%3A16225007011%2Cn%3A172635&dc&page=77&fst=as%3Aoff&qid=1569756866&rnid=16225007011&ref=sr_pg_2']
+    # start_urls = ['https://www.amazon.com/s?i=computers-intl-ship&bbn=16225007011&rh=n%3A16225007011%2Cn%3A172584&dc&page=47&fst=as%3Aoff&qid=1569757528&rnid=16225007011&ref=sr_pg_2']
+    # start_urls = ['https://www.amazon.com/s?i=computers-intl-ship&bbn=16225007011&rh=n%3A16225007011%2Cn%3A11036071&dc&page=2&fst=as%3Aoff&qid=1569758588&rnid=16225007011&ref=sr_pg_2']
+    start_urls = ['https://www.amazon.com/s?i=computers-intl-ship&bbn=16225007011&rh=n%3A16225007011%2Cn%3A2348628011&dc&page=97&fst=as%3Aoff&qid=1569761725&rnid=16225007011&ref=sr_pg_97']
 
     # page_number = 316
     # page_number_total = 400
