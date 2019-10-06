d8e906db4ee92f655ef9276eee58287812707553
Paul Mospan <pavel.mospan@yahoo.com>
2019-10-07 00:18:05 +0300

    Data: amazon_ ind-scien: scrapped a lot

--- data/amazon/amazon/spiders/amazon_spider_industrial_scientific.py
+++ data/amazon/amazon/spiders/amazon_spider_industrial_scientific.py
@@ -16,7 +16,15 @@ class AmazonSpiderIndustrialScientific(scrapy.Spider):
     # start_urls = ['https://www.amazon.com/s?i=industrial-intl-ship&bbn=16225012011&rh=n%3A16225012011%2Cn%3A383598011&dc&page=261&fst=as%3Aoff&qid=1570291687&rnid=16225012011&ref=sr_pg_261']
     # start_urls = ['https://www.amazon.com/s?i=industrial-intl-ship&bbn=16225012011&rh=n%3A%2116225012011%2Cn%3A18746931011&dc&fst=as%3Aoff&qid=1570265229&rnid=16225012011&ref=lp_16225012011_nr_n_7']
     # start_urls = ['https://www.amazon.com/s?i=industrial-intl-ship&bbn=16225012011&rh=n%3A16225012011%2Cn%3A383599011&dc&page=2&fst=as%3Aoff&qid=1570292821&rnid=16225012011&ref=sr_pg_2']
-    start_urls = ['https://www.amazon.com/s?i=industrial-intl-ship&bbn=16225012011&rh=n%3A16225012011%2Cn%3A3061625011&dc&page=290&fst=as%3Aoff&qid=1570297358&rnid=16225012011&ref=sr_pg_290']
+    # start_urls = ['https://www.amazon.com/s?i=industrial-intl-ship&bbn=16225012011&rh=n%3A16225012011%2Cn%3A3061625011&dc&page=290&fst=as%3Aoff&qid=1570297358&rnid=16225012011&ref=sr_pg_290']
+    # start_urls = ['https://www.amazon.com/s?i=industrial-intl-ship&bbn=16225012011&rh=n%3A16225012011%2Cn%3A6054382011&dc&page=2&fst=as%3Aoff&qid=1570305978&rnid=16225012011&ref=sr_pg_2']
+    # start_urls = ['https://www.amazon.com/s?i=industrial-intl-ship&bbn=16225012011&rh=n%3A16225012011%2Cn%3A3021479011&dc&page=2&fst=as%3Aoff&qid=1570314456&rnid=16225012011&ref=sr_pg_2']
+    # start_urls = ['https://www.amazon.com/s?i=industrial-intl-ship&bbn=16225012011&rh=n%3A16225012011%2Cn%3A306506011&dc&page=2&fst=as%3Aoff&qid=1570317179&rnid=16225012011&ref=sr_pg_2']
+    # start_urls = ['https://www.amazon.com/s?i=industrial-intl-ship&bbn=16225012011&rh=n%3A16225012011%2Cn%3A16412251&dc&page=97&fst=as%3Aoff&qid=1570320855&rnid=16225012011&ref=sr_pg_97']
+    # start_urls = ['https://www.amazon.com/s?i=industrial-intl-ship&bbn=16225012011&rh=n%3A16225012011%2Cn%3A317971011&dc&page=333&fst=as%3Aoff&qid=1570360948&rnid=16225012011&ref=sr_pg_333']
+    # start_urls = ['https://www.amazon.com/s?i=industrial-intl-ship&bbn=16225012011&rh=n%3A16225012011%2Cn%3A317970011&dc&page=2&fst=as%3Aoff&qid=1570388109&rnid=16225012011&ref=sr_pg_2']
+    # start_urls = ['https://www.amazon.com/s?i=industrial-intl-ship&bbn=16225012011&rh=n%3A16225012011%2Cn%3A256346011&dc&page=2&fst=as%3Aoff&qid=1570390586&rnid=16225012011&ref=sr_pg_2']
+    start_urls = ['https://www.amazon.com/s?i=industrial-intl-ship&bbn=16225012011&rh=n%3A16225012011%2Cn%3A318135011&dc&page=2&fst=as%3Aoff&qid=1570394024&rnid=16225012011&ref=sr_pg_2']
 
     def parse(self, response):
         items = AmazonItem()
