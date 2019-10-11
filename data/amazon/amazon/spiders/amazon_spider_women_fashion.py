6e42b2b02909c57eb0587d76022253afdaa5b686
Paul Mospan <pavel.mospan@yahoo.com>
2019-10-11 16:13:59 +0300

    Data: amazon: baby: deleted all fucked up spaces

--- data/amazon/amazon/spiders/amazon_spider_women_fashion.py
+++ data/amazon/amazon/spiders/amazon_spider_women_fashion.py
@@ -24,13 +24,14 @@ class AmazonSpiderWomenFashion(scrapy.Spider):
     # start_urls = ['https://www.amazon.com/s?i=fashion-womens-intl-ship&bbn=16225018011&rh=n%3A16225018011%2Cn%3A1040660%2Cn%3A9522932011&dc&page=2&pf_rd_i=16225018011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=0909843d-7d3a-40a7-8b35-6c3da8755bbd&pf_rd_p=0909843d-7d3a-40a7-8b35-6c3da8755bbd&pf_rd_r=4QH1R4R5T751ZQ1FKQAW&pf_rd_r=4QH1R4R5T751ZQ1FKQAW&pf_rd_s=merchandised-search-left-2&pf_rd_t=101&qid=1569270810&rnid=1040660&ref=sr_pg_2']
     # start_urls = ['https://www.amazon.com/Socks-Hosiery-Clothing/s?rh=n%3A1044886&page=255']
     # start_urls = ['https://www.amazon.com/s?i=fashion-womens-intl-ship&bbn=16225018011&rh=n%3A7141123011%2Cn%3A16225018011%2Cn%3A1040660&page=2&pf_rd_i=16225018011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=0909843d-7d3a-40a7-8b35-6c3da8755bbd&pf_rd_p=0909843d-7d3a-40a7-8b35-6c3da8755bbd&pf_rd_r=7YWR4MMQ0JXW2JFXPGQ9&pf_rd_r=7YWR4MMQ0JXW2JFXPGQ9&pf_rd_s=merchandised-search-left-2&pf_rd_t=101&qid=1569277142&ref=sr_pg_2']
-    start_urls = ['']
+    # start_urls = ['https://www.amazon.com/s?i=fashion-womens-intl-ship&bbn=16225018011&rh=n%3A16225018011%2Cn%3A679337011%2Cn%3A6127771011&dc&pf_rd_i=16225018011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=0909843d-7d3a-40a7-8b35-6c3da8755bbd&pf_rd_p=0909843d-7d3a-40a7-8b35-6c3da8755bbd&pf_rd_r=28TCGP4Q4HJN0G7G31XW&pf_rd_r=28TCGP4Q4HJN0G7G31XW&pf_rd_s=merchandised-search-left-2&pf_rd_t=101&qid=1570790472&rnid=679337011&ref=sr_nr_n_1']
+    start_urls = ['https://www.amazon.com/s?i=fashion-womens-intl-ship&bbn=16225018011&rh=n%3A16225018011%2Cn%3A679337011%2Cn%3A679380011&dc&page=2&pf_rd_i=16225018011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=0909843d-7d3a-40a7-8b35-6c3da8755bbd&pf_rd_p=0909843d-7d3a-40a7-8b35-6c3da8755bbd&pf_rd_r=28TCGP4Q4HJN0G7G31XW&pf_rd_r=28TCGP4Q4HJN0G7G31XW&pf_rd_s=merchandised-search-left-2&pf_rd_t=101&qid=1570793471&rnid=679337011&ref=sr_pg_2']
 
     def parse(self, response):
         items = AmazonItem()
         next_page_real = response.css(".a-last a").css("::attr(href)").get()
 
-        ad = response.css('.a-color-base.a-text-normal::text').extract()
+        ad = response.css('.a-size-base-plus::text').extract()
 
         for i in range(len(ad)):
             items['ad'] = ad[i]
